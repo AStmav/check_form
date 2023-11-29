a56d54678a0d2253fb8a136be8594d79b99d710c
Ast <mashruin@ngs.ru>
2023-11-29 23:01:41 +0300

    first commit

---
+++ venv/web_app_check_form/check_app/tests.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+
+
+
+class TolokaAccountKeysTestCase(TestCase):
+    form_url: str = '/get_form'
+
+
+
+    def test_bad_result_sravnenie_form_s_validaciey_dannyh(self):
+        response = self.client.post(self.form_url, data={
+            "field_name_1": 'infodo@edu.mos.ru',
+
+
+        })
+
+        self.assertEqual(response.status_code, 400)
+
+
+
+    def test_ok_result_sravnenie_form_s_validaciey_dannyh(self):
+        response = self.client.post(self.form_url, data={
+            "field_name_1": 'abc@mail.ru',
+            "field_name_2": '+7 926 520 97 64'
+
+        })
+
+        self.assertEqual(response.status_code, 200)
+        response_data = response.json()
+        self.assertEqual(response_data["name_form_template"], "Form template name 5")
+
+    def test_ok_result_vyvod_informacii_o_dannyh_ne_proshedshih_validaciu_1(self):
+        response = self.client.post(self.form_url, data={
+            "field_name_1": 'abcmail.ru',
+            "field_name_2": '+ 926 520 97 64'
+        })
+
+        self.assertEqual(response.status_code, 400)
+
+
+    def test_ok_result_vyvod_informacii_o_dannyh_ne_proshedshih_validaciu_2 (self):
+        response = self.client.post(self.form_url, data={
+            "field_name_1": 'abc@mail.ru',
+            "field_name_2": '+ 926 520 97 64'
+        })
+
+        self.assertEqual(response.status_code, 400)
---
+++ venv/web_app_check_form/check_app/views.py
@@ -0,0 +1,90 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponseBadRequest, JsonResponse
+from django.urls import reverse
+import json
+import re
+
+
+def form_list(request):
+    return render(request, "check_app/form.html")
+
+def check_form(request):
+    if request.method == 'POST':
+        query_post = request.POST
+        keys_query = list(query_post.keys())
+        keys_query = [
+            key for key in keys_query if key != 'csrfmiddlewaretoken'
+        ]
+
+        file_json = open("db.json", encoding="utf8")
+        list_json = json.load(file_json)
+        current_template = None
+        keys_count = 0
+        for form_template in list_json:
+            current_template_field_set = set(form_template.keys())
+            current_template_field_set.remove('name')
+            if current_template_field_set <= set(keys_query):
+                current_form_keys_count = len(current_template_field_set)
+                if current_form_keys_count > keys_count:
+                    current_template = form_template
+
+        if current_template is None:
+
+            return JsonResponse({'reason': "form not found"}, status=400)
+
+        format_fields = []
+        for field_name, field_type in current_template.items():
+            if field_name == 'name':
+                continue
+            input_value = query_post[field_name]
+            if field_type == 'date':
+
+                pattern = r'(?<!\d)(?:0?[1-9]|[12][0-9]|3[01]).(?:0?[1-9]|1[0-2]).(?:21[0-9][0-9]|20[01][0-9])(?!\d)'
+                pattern1 = r'(?<!\d)(?:21[0-9][0-9]|20[01][0-9])-(?:0?[1-9]|1[0-2])-(?:0?[1-9]|[12][0-9]|3[01])(?!\d)'
+                result = bool(re.findall(pattern, input_value))
+                result1 = bool(re.findall(pattern1, input_value))
+                if result is True or result1 is True:
+
+                    continue
+                else:
+
+                    format_date = (field_name, field_type)
+                    format_fields.append(format_date)
+
+            input_value = query_post[field_name]
+            if field_type == 'phone':
+
+                pattern = re.compile(r"(\+7)[\s]\d{3}[\s]\d{3}[\s]\d{2}[\s]\d{2}$")
+                result = bool (re.search(pattern, input_value))
+                if result is True:
+
+                    continue
+                else:
+
+                    format_phone = (field_name, field_type)
+                    format_fields.append(format_phone)
+
+            input_value = query_post[field_name]
+            if field_type == 'email':
+                pattern = r"^[-\w\.]+@([-\w]+\.)+[-\w]{2,4}$"
+                result = bool(re.findall(pattern, input_value))
+                if result is True:
+
+                    continue
+                else:
+
+                    format_email = (field_name, field_type)
+                    format_fields.append(format_email)
+
+
+        dict_bad_format = dict(format_fields)
+        list_correct_form = [("name_form_template", current_template['name'])]
+        dict_good_form = dict(list_correct_form)
+
+        if not dict_bad_format :
+
+            return JsonResponse(dict_good_form)
+        else:
+            return JsonResponse(dict_bad_format, status=400)
+
+    return HttpResponseRedirect("/")
